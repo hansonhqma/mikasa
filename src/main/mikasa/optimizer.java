50dae52c82f827df2cd39dd259be95ae0139935d
Hanson Ma <hanson.hq.ma@gmail.com>
2021-07-30 21:25:02 -0400

    method stubs

---
+++ src/main/mikasa/optimizer.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class optimizer {
+
+    private int[] discretive_vector(int[] degree_vector){
+
+        throw new RuntimeException("unfinished");
+    }
+
+    private double sse(int[] degree_vector, int[] capacity_vector){
+
+        throw new RuntimeException("unfinished");
+    }
+
+    private int[] gradient(int[] degree_vector, int[] capacity_vector){
+
+        throw new RuntimeException("unfinished");
+    }
+
+    private int[] mm_normalize(int[] vec){
+
+        throw new RuntimeException("unfinished");
+    }
+
+    public HashMap<Integer, ArrayList<Integer>> solve(int [] capacity_vector){
+
+        throw new RuntimeException("unfinished");
+    }
+}
