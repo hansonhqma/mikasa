a277b1ef7d055923b0679b0d5c65522330bbac47
Hanson Ma <hanson.hq.ma@gmail.com>
2021-08-02 00:27:50 -0400

    optimizer implemented

--- src/main/mikasa/optimizer.java
+++ src/main/mikasa/optimizer.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.math.*;
 
 public class optimizer {
+    private static final double ZERO = 0.00001;
 
-    public optimizer(){
+    public optimizer(){}
 
-    }
-
-    private double min(double[] arr){
+    /**
+     * @requires arr.length>0
+     * @param arr
+     * @return
+     */
+    public static double min(double[] arr){
         double m = arr[0];
         for(int i=1;i<arr.length;++i){
             if(arr[i]<m){m=arr[i];}
@@ -16,7 +20,12 @@ public class optimizer {
         return m;
     }
 
-    private double max(double[] arr){
+    /**
+     * @requires arr.length>0
+     * @param arr
+     * @return
+     */
+    public static double max(double[] arr){
         double m = arr[0];
         for(int i=1;i<arr.length;++i){
             if(arr[i]>m){m=arr[i];}
@@ -24,7 +33,36 @@ public class optimizer {
         return m;
     }
 
-    public double[] discretive_vector(double[] degree_vector){
+    public static int argmax(double[] array) {
+        double max = array[0];
+        int re = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                re = i;
+            }
+        }
+        return re;
+    }
+
+    public static int argmin(double[] array) {
+        double min = array[0];
+        int re = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                re = i;
+            }
+        }
+        return re;
+    }
+
+    /**
+     * @requires degree_vector!=null
+     * @param degree_vector
+     * @return
+     */
+    public static double[] discretize_vector(double[] degree_vector){
         int n = degree_vector.length;
         double[] delta_vector = new double[n];
         for(int i=0;i<n;++i){ // construct delta vector
@@ -49,7 +87,12 @@ public class optimizer {
         return degree_vector;
     }
 
-    public double[] mm_normalize(double[] vec){
+    /**
+     * @requires vec.length>0
+     * @param vec
+     * @return
+     */
+    public static double[] mm_normalize(double[] vec){
         double lb = min(vec);
         double ub = max(vec);
         for(int i=0;i<vec.length;++i){
@@ -58,7 +101,13 @@ public class optimizer {
         return vec;
     }
 
-    public double sse(double[] degree_vector, double[] capacity_vector){
+    /**
+     * @requires capacity_vector.length>0 && capacity_vector.length == degree_vector.length
+     * @param degree_vector
+     * @param capacity_vector
+     * @return
+     */
+    public static double sse(double[] degree_vector, double[] capacity_vector){
         capacity_vector = mm_normalize(capacity_vector);
         int n = degree_vector.length;
         double degree_max = max(degree_vector);
@@ -69,13 +118,135 @@ public class optimizer {
         return sum/(n-1);
     }
 
-    public int[] gradient(int[] degree_vector, int[] capacity_vector){
+    /**
+     * @requires capacity_vector.length>1 && capacity_vector.length == degree_vector.length
+     * @param degree_vector
+     * @param capacity_vector
+     * @return
+     */
+    public static double[] gradient(double[] degree_vector, double[] capacity_vector){
+        capacity_vector = mm_normalize(capacity_vector);
+        int n = degree_vector.length;
+        double degree_max = max(degree_vector);
 
-        throw new RuntimeException("unfinished");
+        double[] gradient_vector = new double[n];
+        // first term
+        gradient_vector[0]=-(2*(capacity_vector[1] - capacity_vector[0] + (degree_vector[0] - degree_vector[1])/degree_max))/(degree_max*(n - 1));
+        // middle terms
+        for(int i=1;i<n-1;++i){
+            gradient_vector[i]=((2*(capacity_vector[i] - capacity_vector[i-1] + (degree_vector[i-1] - degree_vector[i])/degree_max))/degree_max - (2*(capacity_vector[i+1] - capacity_vector[i] + (degree_vector[i] - degree_vector[i+1])/degree_max))/degree_max)/(n - 1);
+        }
+        // final term
+        gradient_vector[n-1]=(2*(capacity_vector[n-1] - capacity_vector[n-2] + (degree_vector[n-2] - degree_vector[n-1])/degree_max))/(degree_max*(n - 1));
+        // get rid of floating point inaccuracy for zero
+        for(int i=0;i<n;++i){
+            if(Math.abs(gradient_vector[i])<ZERO){gradient_vector[i]=0;}
+        }
+
+        return gradient_vector;
     }
 
-    public HashMap<Integer, ArrayList<Integer>> solve(int [] capacity_vector){
+    /**
+     * @requires capacity_vector.length>1
+     * @param capacity_vector
+     * @return
+     */
+    public static double[] solve_degree(double[] capacity_vector){
+        int n = capacity_vector.length;
+        capacity_vector = mm_normalize(capacity_vector);
+        int sum_deg = 2*n-2;
 
-        throw new RuntimeException("unfinished");
+        double[] degree_vector = new double[n];
+        degree_vector[0]=1;degree_vector[1]=1;
+        for(int i=2;i<n;++i){degree_vector[i]=2;} // fill init degree vector
+
+        double error_previous = sse(degree_vector, capacity_vector);
+        double[] degree_previous = new double[n];
+        for(int i=0;i<n;++i){degree_previous[i]=degree_vector[i];} // fill previous vector
+
+        while(true){
+            double[] gradient_vector = gradient(degree_vector, capacity_vector);
+            if(gradient_vector[0]>0){
+                for(int i=1;i<n;++i){
+                    if(gradient_vector[i]<0){break;}
+                    if(i==n-1){return discretize_vector(degree_vector);}
+                }
+            }else if(gradient_vector[0]<0){
+                for(int i=1;i<n;++i){
+                    if(gradient_vector[i]>0){break;}
+                    if(i==n-1){return discretize_vector(degree_vector);}
+                }
+            }else{
+                for(int i=1;i<n;++i){
+                    if(gradient_vector[i]!=0){break;}
+                    if(i==n-1){return discretize_vector(degree_vector);}
+                }
+            } // gradient consistency checks
+
+            int amax = argmax(gradient_vector);
+            int amin = argmin(gradient_vector);
+
+            if(degree_vector[amin]==1 || degree_vector[amax]==n-1){discretize_vector(degree_vector);}
+            // make changes
+            degree_vector[amax] += 0.03125;
+            degree_vector[amin] -= 0.03125; // ensures no absolute change
+
+            double error = sse(degree_vector, capacity_vector);
+            if(error>=error_previous){return discretize_vector(degree_previous);} // no improvement, return previous
+
+            error_previous = error;
+            for(int i=0;i<n;++i){degree_previous[i]=degree_vector[i];} // set previous degree vector
+        }
+    }
+
+    /**
+     * @requires capacity_vector.length>0
+     * @param capacity_vector
+     * @return
+     */
+    public static HashMap<Integer, ArrayList<Integer>> solve(double[] capacity_vector) {
+        int n = capacity_vector.length;
+        HashMap<Integer, ArrayList<Integer>> solution = new HashMap<>();
+
+        double[] degree_vector = solve_degree(capacity_vector);
+        boolean[] in_graph = new boolean[n];
+        double[] ZERO = new double[n];
+        for (int i = 0; i < n; ++i) { // init empty edges and in_graph array
+            solution.put(i, new ArrayList<>());
+            in_graph[i] = false; // non
+            ZERO[i] = 0; // define zero vector
+        }
+
+        while(!Arrays.equals(degree_vector, ZERO)){
+            int parent = argmax(degree_vector);
+            while(degree_vector[parent]!=0){
+                double[] selection_vector = new double[n];
+                int child;
+                if(in_graph[parent]){
+                    for(int i=0;i<n;++i){
+                        if(in_graph[i]){selection_vector[i]=0;}
+                        else{selection_vector[i]=degree_vector[i];}
+                    }
+                    selection_vector[parent]=0;
+                    child = argmax(selection_vector);
+                    in_graph[child]=true;
+                }else{
+                    for(int i=0;i<n;++i){
+                        if(!in_graph[i]){selection_vector[i]=0;}
+                        else{selection_vector[i]=degree_vector[i];}
+                    }
+                    selection_vector[parent]=0;
+                    child = argmax(selection_vector);
+                    in_graph[child]=true;
+                    in_graph[parent]=true;
+                }
+                solution.get(parent).add(child);
+                solution.get(child).add(parent); // add to solution
+
+                degree_vector[parent] -= 1;
+                degree_vector[child] -= 1;
+            }
+        }
+        return solution;
     }
 }
--- src/test/mikasa/optimizerTests.java
+++ src/test/mikasa/optimizerTests.java
@@ -1,12 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public final class optimizerTests {
+import java.util.*;
+
+public final class optimizerTests{
 
     @Test
     public void discretizer_test(){
-        optimizer demo  = new optimizer();
-        double[] out = demo.discretive_vector(new double[]{1.25,1.125,1.875,2.125,1.625});
+        double[] out = optimizer.discretize_vector(new double[]{1.25,1.125,1.875,2.125,1.625});
         assertTrue(out[0]==1);
         assertTrue(out[1]==1);
         assertTrue(out[2]==2);
@@ -16,9 +17,8 @@ public final class optimizerTests {
 
     @Test
     public void normalizerTest(){
-        optimizer demo  = new optimizer();
         double[] actual = new double[]{0.0, 0.25, 0.5, 0.75, 1.0};
-        double[] normalized = demo.mm_normalize(new double[]{1,2,3,4,5});
+        double[] normalized = optimizer.mm_normalize(new double[]{1,2,3,4,5});
         for(int i=0;i<normalized.length;++i){
             assertTrue(normalized[i]==actual[i]);
         }
@@ -26,10 +26,32 @@ public final class optimizerTests {
 
     @Test
     public void sseTest(){
-        optimizer demo  = new optimizer();
         double[] d = new double[]{1,1,2,2,2};
         double[] c = new double[]{1,2,3,4,5};
-        double e = demo.sse(d, c);
+        double e = optimizer.sse(d, c);
         assertTrue(e==0.0625);
+    }
+
+    @Test
+    public void gradientTest(){
+        double[] d = new double[]{1,1,2,2,2};
+        double[] c = new double[]{1,2,3,4,5};
+        double[] g = optimizer.gradient(d,c);
+        double[] actual = new double[]{-0.0625, 0.125, -0.125, 0, 0.0625};
+        assertTrue(Arrays.equals(g, actual));
+    }
+
+    @Test
+    public void solveDegreeTest(){
+        double[] c = new double[]{1,1,2,3,6};
+        double[] actual = new double[]{1,1,1,2,3};
+        assertTrue(Arrays.equals(actual, optimizer.solve_degree(c)));
+    }
+
+    @Test
+    public void solveNetworkTest(){
+        double[] c = new double[]{1,3,1,2,5,15};
+        HashMap<Integer, ArrayList<Integer>> s = optimizer.solve(c);
+        System.out.println(s); //  not really a test but whatever
     }
 }
